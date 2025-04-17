a0abd60ca9b163bca63f600186634f065839bb4e
dtrognn <dtrognn@gmail.com>
2025-04-17 18:20:04 +0700

    Initial project

---
+++ SwiftUICoordinator/SwiftUICoordinator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICoordinator
+//
+//  Created by dtrognn on 17/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICoordinator/SwiftUICoordinator/SwiftUICoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorApp.swift
+//  SwiftUICoordinator
+//
+//  Created by dtrognn on 17/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
